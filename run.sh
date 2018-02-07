bd2340080bd5465e77d27dbb48c975a75022704f
isa <isa@isa>
2018-02-07 15:37:35 +0800

    commit

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
  nohup /google_appengine/dev_appserver.py --skip_sdk_update_check --host=0.0.0.0  /WebRTC/apprtc/out/app_engine &> apprtc.log &
 
 /WebRTC/collider_root/bin/collidermain -port=8089 -tls=false -room-server=http://localhost:8080 
